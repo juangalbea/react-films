9d1300e0e32a515b82809163c81ffbd66c723241
juangalbea <juangalbea@gmail.com>
2020-02-17 23:50:30 +0100

    movies list

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import films from '../films.json';
+import { Link } from 'react-router-dom';
+import { Route} from 'react-router-dom';
 
 export class Movies extends Component {
     render() {
         return (
             <div>
-                <h2>Movies list</h2>
+                <div id="root">
+                    <div>
+                        <nav className="navbar navbar-dark bg-primary mb-3">
+                            <div className="container">
+                                <a className="navbar-brand" href="/">Movie List</a>
+                            </div>
+                        </nav>
+                        <div className="container">
+                            <div className="row">
+                            <div className="col-5" style={{ maxHeight: '80vh', overflow: 'scroll' }}>
+                                    <div className="list-group">
+                                        {films.map((film, idx) =>  { return (
+                    <a className="list-group-item list-group-item-action" style={{fontSize: "25px"}} href="/AFG">{film.Title}</a>
+                  )})}
+                                      
+                                    </div>
+                                </div>
+                              
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
