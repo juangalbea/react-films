4896e04e10fc10db89a664552399172e6c038ca0
juangalbea <juangalbea@gmail.com>
2020-06-20 18:32:23 +0200

    Updates

---
+++ precache-manifest.22c32f0a81833fc0a7f24bdd0a4f19d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e2450ca553653b9bce28dc35b8180b",
+    "url": "/react-films/index.html"
+  },
+  {
+    "revision": "2b7a8433b0f1fb7fed8f",
+    "url": "/react-films/static/css/2.b4687569.chunk.css"
+  },
+  {
+    "revision": "ce14e84603114ff99f5e",
+    "url": "/react-films/static/css/main.5866df4b.chunk.css"
+  },
+  {
+    "revision": "2b7a8433b0f1fb7fed8f",
+    "url": "/react-films/static/js/2.83d7155f.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react-films/static/js/2.83d7155f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce14e84603114ff99f5e",
+    "url": "/react-films/static/js/main.b3356f96.chunk.js"
+  },
+  {
+    "revision": "a4cc9cfa29919e59576e",
+    "url": "/react-films/static/js/runtime-main.7c51c55f.js"
+  },
+  {
+    "revision": "331330a5ff34469ad3acafedb6d5f838",
+    "url": "/react-films/static/media/film-1668918_1280.331330a5.jpg"
+  },
+  {
+    "revision": "a04ae4bc660c53473c267079483a0aa4",
+    "url": "/react-films/static/media/movie-918655_1280.a04ae4bc.jpg"
+  }
+]);
