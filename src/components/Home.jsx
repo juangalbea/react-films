b8fcac56435b5a2758c30d8cc9fb16090833cdff
juangalbea <juangalbea@gmail.com>
2020-11-11 23:40:02 +0000

    responsive image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
+import '../App.css';
+
 export class Home extends Component {
     render() {
         return (
-            <div>
+            <div id="home">
                 <h1 style={{fontSize: "60px", margin: "30px"}}>The films in <b>React</b></h1>
                 <img src={require('../img/film-1668918_1280.jpg')} alt=""/>
             </div>
